01cb0d2336aff02ad3d963cba7fe87bdfb25efcf
Ezequiel Juliano Müller <ezequieljuliano@gmail.com>
2014-07-15 18:22:15 -0300

    Add MediaType

--- src/REST4D.pas
+++ src/REST4D.pas
@@ -21,6 +21,24 @@ type
     class function HashSHA1(const pSource: string): string; static;
   end;
 
+  TRESTMediaType = class sealed
+  public
+    const
+    APPLICATION_ATOM_XML = 'application/atom+xml';
+    APPLICATION_FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    APPLICATION_JSON = 'application/json';
+    APPLICATION_OCTET_STREAM = 'application/octet-stream';
+    APPLICATION_SVG_XML = 'application/svg+xml';
+    APPLICATION_XHTML_XML = 'application/xhtml+xml';
+    APPLICATION_XML = 'application/xml';
+    MEDIA_TYPE_WILDCARD = '*';
+    MULTIPART_FORM_DATA = 'multipart/form-data';
+    TEXT_HTML = 'text/html';
+    TEXT_PLAIN = 'text/plain';
+    TEXT_XML = 'text/xml';
+    WILDCARD = '*/*';
+  end;
+
   IRESTUserAuthenticate = interface
     ['{56A84057-56BB-4742-8ED2-6C5D47CA3E3A}']
     function GetUserName(): string;
@@ -116,7 +134,7 @@ type
     function UserIsValid(const pApiKey: string): Boolean; overload;
   end;
 
-{ TRESTUserAuthenticate }
+  { TRESTUserAuthenticate }
 
 constructor TRESTUserAuthenticate.Create;
 begin
